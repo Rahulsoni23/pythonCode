1fd403d56781e39296719c44df2dbbe521c78990
Rahul Soni <104891181+Rahulsoni23@users.noreply.github.com>
2022-05-27 17:25:00 +0530

    Add files via upload
    Swapping Using Thrid Variable

---
+++ swapping.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 17:20:40 2022
+
+@author: PRIYA
+"""
+
+# swapping of two number
+a=5
+b=6
+temp=0
+temp=a
+a=b
+print("variable a:" ,temp)
+print("variable b :",a )
+
