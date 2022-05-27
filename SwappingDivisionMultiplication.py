ab5d50cd3d7a9af6acdcc67c24f322290572f0d5
Rahul Soni <104891181+Rahulsoni23@users.noreply.github.com>
2022-05-27 17:27:24 +0530

    Add files via upload
    Swapping By Division & Multiplication

---
+++ SwappingDivisionMultiplication.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 17:21:19 2022
+
+@author: PRIYA
+"""
+
+# swapping of two number with % and *
+a=4
+b=2
+a=a*b
+b=a/b
+print("after swapping b:",b)
+a=a/b
+print("after swapping a:",a)
