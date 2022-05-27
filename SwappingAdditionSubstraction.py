6eeebfabad31ec285a9a0ce409e6f9da2f23cca6
Rahul Soni <104891181+Rahulsoni23@users.noreply.github.com>
2022-05-27 17:26:12 +0530

    Add files via upload
    Swapping By Addition & Substraction

---
+++ SwappingAdditionSubstraction.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 27 17:21:00 2022
+
+@author: PRIYA
+"""
+
+# swapping of two number with addition & subtraction
+a=6
+b=5
+a=a+b
+b=a-b
+print("after swapping b   : ",b)
+a=a-b
+print("after swapping a  :  ",a)
+
+
