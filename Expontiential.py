d6ac8bdfe2844d48ebaa1e53e5ed21444c6a2dd3
Rahul Soni <104891181+Rahulsoni23@users.noreply.github.com>
2022-06-02 21:43:30 +0530

    Add files via upload
    Exponential By Using Function

---
+++ Expontiential.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  2 21:39:57 2022
+
+@author: PRIYA
+"""
+
+
+def exp (base,power):
+     
+     if (power==1):
+         return power
+     else:
+         return base**power 
+         print(base**power)
+exp(4,3)
